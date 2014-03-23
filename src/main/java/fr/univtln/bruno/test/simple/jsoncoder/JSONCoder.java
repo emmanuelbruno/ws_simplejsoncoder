01e2b376998101ec68360faa01c0416620fdeb49
Emmanuel Bruno <bruno@univ-tln.fr>
2014-03-23 15:33:29 +0100

    Initial release

---
+++ src/main/java/fr/univtln/bruno/test/simple/jsoncoder/JSONCoder.java
@@ -0,0 +1,51 @@
+package fr.univtln.bruno.test.simple.jsoncoder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * Created by bruno on 23/03/14.
+ */
+public class JSONCoder<T> implements  Encoder.TextStream<T>, Decoder.TextStream<T>{
+
+    private static final ObjectMapper _objectMapper = new ObjectMapper();
+    private ObjectWriter _writer = JSONCoder._objectMapper.writer();
+    private ObjectReader _reader = JSONCoder._objectMapper.reader();
+
+    private Class<T> _type;
+
+    @Override
+    public void init(EndpointConfig endpointConfig) {
+        ParameterizedType $thisClass = (ParameterizedType) this.getClass().getGenericSuperclass();
+        Type $T = $thisClass.getActualTypeArguments()[0];
+        if ($T instanceof Class) {
+            _type = (Class<T>)$T;
+        }
+        else if ($T instanceof ParameterizedType) {
+            _type = (Class<T>)((ParameterizedType)$T).getRawType();
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void encode(T t, Writer writer) throws EncodeException, IOException {
+        _objectMapper.writeValue(writer, t);
+    }
+
+    @Override
+    public T decode(Reader reader) throws DecodeException, IOException {
+        return _objectMapper.readValue(reader, _type);
+    }
+}
